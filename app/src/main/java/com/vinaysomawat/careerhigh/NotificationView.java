c49d6d9747ca9e077536b9fd63cdb45c68ce4ce5
somawatvinay <vinaysomawat40@gmail.com>
2019-01-10 17:31:01 +0530

    added launching activity

---
+++ app/src/main/java/com/vinaysomawat/careerhigh/NotificationView.java
@@ -0,0 +1,16 @@
+package com.vinaysomawat.careerhigh;
+
+/**
+ * Created by Vinay Somawat on 10-01-2019.
+ */
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class NotificationView extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+    }
+}
---
+++ app/src/main/java/com/vinaysomawat/careerhigh/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.vinaysomawat.careerhigh;
+
+/**
+ * Created by Vinay Somawat on 10-01-2019.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
