25718d383c9b3610ae532af8e420a2c4a3a56bdb
somawatvinay <vinaysomawat40@gmail.com>
2019-01-10 13:38:32 +0530

    initial commit

---
+++ app/src/main/java/com/vinaysomawat/careerhigh/MainActivity.java
@@ -0,0 +1,50 @@
+package com.vinaysomawat.careerhigh;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends ActionBarActivity {
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview = (WebView)findViewById(R.id.webView);
+        WebSettings webSettings = mywebview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://careerhigh.in");
+        mywebview.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(mywebview.canGoBack()) {
+            mywebview.goBack();
+        } else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if(id == R.id.action_settings){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
